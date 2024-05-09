cf681b20f508e225b4739e256dd5086e67631408
mosguinz <mos.guinz@gmail.com>
2024-05-09 14:39:12 -0700

    Update Makefile

---
+++ Test/Kullathon_Mos_HW6_main.c
@@ -0,0 +1,19 @@
+/**************************************************************
+ * Class::  CSC-415-03 Spring 2024
+ * Name::  Mos Kullathon
+ * Student ID::  921425216
+ * GitHub-Name::  mosguinz
+ * Project::  Assignment 6 – Device Driver
+ *
+ * File::  Mos_Kullathon_HW6_main.c
+ *
+ * Description::  Device driver test application.
+ *
+ **************************************************************/
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    printf("bruh\n");
+    return 0;
+}
