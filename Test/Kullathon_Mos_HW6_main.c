4a3db18cd0d8dc5f0becb572fe88cc051223e066
mosguinz <mos.guinz@gmail.com>
2024-05-09 22:20:21 -0700

    Add test driver file

--- Test/Kullathon_Mos_HW6_main.c
+++ Test/Kullathon_Mos_HW6_main.c
@@ -11,9 +11,41 @@
  *
  **************************************************************/
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <sys/ioctl.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <errno.h>
+
+#define READ_BUFFER_SIZE 512
+#define DEV_LOCATION "/dev/salad"
 
 int main(int argc, char const *argv[])
 {
-    printf("bruh\n");
+    int fd, info;
+    long n1, n2, n3;
+    char Message[] = {"test message"};
+    char read_buffer[READ_BUFFER_SIZE];
+
+    fd = open(DEV_LOCATION, O_WRONLY);
+    printf("Returned from open(): %d\n", fd);
+
+    if (fd < 0)
+    {
+        printf("Device open error\n");
+        perror("Device file open error");
+        return -1;
+    }
+    printf("Device open success\n");
+
+    for (long i = 0; i < 30; i++)
+    {
+        n1 = write(fd, "testing", i * 2);
+        n2 = ioctl(fd, 3, &info);
+        printf("Wrote to %d, that returned %ld, count %d - Return from ioctl %i\n", fd, n1, i, n2);
+    }
+
+    close(fd);
     return 0;
 }
